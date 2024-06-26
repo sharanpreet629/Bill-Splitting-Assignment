7b997dc0af9c29d1bb7955a6d355f986348932a1
Sharanpreet Kaur <83025267+sharanpreet629@users.noreply.github.com>
2024-06-26 16:39:14 +0530

    Add files via upload

--- run_project.cmd
+++ run_project.cmd
@@ -1,6 +1,7 @@
 @echo off
 echo Running Django Project...
-call venv\Scripts\activate
+call venv1\Scripts\activate
 cd Splitt_bills
 python manage.py runserver
 
+pause
---
+++ setup_venv.cmd
@@ -0,0 +1,10 @@
+
+pip install virtualenv
+
+virtualenv venv1
+
+call venv1\Scripts\activate
+
+pip install -r requirements.txt
+
+pause
